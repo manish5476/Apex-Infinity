16739d6e5839cf72146fd253ffce79440b1adef4
Manish Singh <100526847+manish5476@users.noreply.github.com>
2026-02-23 14:18:05 +0530

    Create permissions.token.ts

---
+++ src/app/core/tokens/permissions.token.ts
@@ -0,0 +1,8 @@
+// src/app/core/tokens/permissions.token.ts
+import { InjectionToken } from '@angular/core';
+import { PERMISSIONS } from '../../../config/permissions';
+
+export const APP_PERMISSIONS = new InjectionToken('AppPermissions', {
+  providedIn: 'root',
+  factory: () => PERMISSIONS
+});
