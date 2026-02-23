5dd5b2e7ff346fbe48094dca55c8cc83e4b8b127
Manish Singh <100526847+manish5476@users.noreply.github.com>
2026-02-23 14:38:04 +0530

    Update permissions.token.ts

--- src/app/core/tokens/permissions.token.ts
+++ src/app/core/tokens/permissions.token.ts
@@ -1,8 +1,8 @@
-// src/app/core/tokens/permissions.token.ts
-import { InjectionToken } from '@angular/core';
-import { PERMISSIONS } from '../../../config/permissions';
+// // src/app/core/tokens/permissions.token.ts
+// import { InjectionToken } from '@angular/core';
+// import { PERMISSIONS } from '../../../config/permissions';
 
-export const APP_PERMISSIONS = new InjectionToken('AppPermissions', {
-  providedIn: 'root',
-  factory: () => PERMISSIONS
-});
+// export const APP_PERMISSIONS = new InjectionToken('AppPermissions', {
+//   providedIn: 'root',
+//   factory: () => PERMISSIONS
+// });
