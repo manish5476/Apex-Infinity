951a6501e4f7b9e58568799107fbf33f43f2c968
Manish Singh <100526847+manish5476@users.noreply.github.com>
2026-04-02 14:31:38 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,7 +16,7 @@ import { providePrimeNG } from 'primeng/config';
 import { MessageService, ConfirmationService } from 'primeng/api';
 
 // Import your custom preset
-// import { MyPreset } from './core/config/my-preset';
+import { MyPreset } from './core/config/my-preset';
 
 // Interceptors
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
@@ -37,21 +37,30 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideClientHydration(),
     provideAnimationsAsync(),
-
+providePrimeNG({
+  ripple: true,
+  theme: {
+    preset: ApexPreset,
+    options: {
+      darkModeSelector: '.theme-dark, .theme-neon-eclipse, ...',
+      cssLayer: { name: 'primeng', order: 'theme, base, primeng' }
+    }
+  }
+})
     // PRIME NG CONFIGURATION
-    providePrimeNG({
-      ripple: true,
-      theme: {
-        // preset: MyPreset, 
-        options: {
-          darkModeSelector: '.theme-dark',
-          cssLayer: {
-            name: 'primeng',
-            order: 'tailwind-base, primeng, tailwind-utilities'
-          }
-        }
-      }
-    }),
+    // providePrimeNG({
+    //   ripple: true,
+    //   theme: {
+    //     preset: MyPreset, 
+    //     options: {
+    //       darkModeSelector: '.theme-dark',
+    //       cssLayer: {
+    //         name: 'primeng',
+    //         order: 'tailwind-base, primeng, tailwind-utilities'
+    //       }
+    //     }
+    //   }
+    // }),
     // ConfirmationService
     MessageService, ConfirmationService,
     DatePipe, DialogService,
