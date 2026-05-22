e7d89dd153321e8acfbec7ac3d8d3907f88a72e5
Manish Singh <100526847+manish5476@users.noreply.github.com>
2026-05-22 14:38:12 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
-
 import { ApplicationConfig, provideAppInitializer, inject } from '@angular/core';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
-import { provideRouter, RouteReuseStrategy } from '@angular/router';
+// ✅ ADDED withHashLocation HERE
+import { provideRouter, RouteReuseStrategy, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration, withEventReplay, withHttpTransferCacheOptions, withIncrementalHydration } from '@angular/platform-browser';
@@ -30,7 +30,10 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([jwtInterceptor, successInterceptor, errorInterceptor]),//LoadingInterceptor
       withFetch()
     ),
-    provideRouter(routes),
+    
+    // ✅ ADDED withHashLocation() HERE
+    provideRouter(routes, withHashLocation()), 
+    
     provideClientHydration(
       withEventReplay(),
       withIncrementalHydration(),
@@ -41,22 +44,12 @@ export const appConfig: ApplicationConfig = {
     ),
     provideZonelessChangeDetection(),
     provideAnimationsAsync(),
-    // providePrimeNG({
-    //   ripple: true,
-    //   theme: {
-    //     preset: ApexPreset,
-    //     options: {
-    //       darkModeSelector: '.theme-dark, .theme-neon-eclipse, ...',
-    //       cssLayer: { name: 'primeng', order: 'theme, base, primeng' }
-    //     }
-    //   }
-    // })
+    
     { provide: RouteReuseStrategy, useClass: TabReuseStrategy },
 
     providePrimeNG({
       ripple: true,
       theme: {
-        // preset: MyPreset, 
         options: {
           darkModeSelector: '.theme-dark',
           cssLayer: {
@@ -66,55 +59,26 @@ export const appConfig: ApplicationConfig = {
         }
       }
     }),
-    // ConfirmationService
     MessageService, ConfirmationService,
     DatePipe, DecimalPipe, PercentPipe, DialogService,
 
-    // ✅ THE MODERN FIX: Using provideAppInitializer
     provideAppInitializer(() => {
       const auth = inject(AuthService);
       return auth.initializeFromStorage();
     })
   ]
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // import { ApplicationConfig, provideAppInitializer, inject } from '@angular/core';
 // import { provideZonelessChangeDetection } from '@angular/core';
 // import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
-// import { provideRouter } from '@angular/router';
+// import { provideRouter, RouteReuseStrategy } from '@angular/router';
 // import { routes } from './app.routes';
-// import { provideClientHydration } from '@angular/platform-browser';
 // import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-// import { DatePipe } from '@angular/common';
+// import { provideClientHydration, withEventReplay, withHttpTransferCacheOptions, withIncrementalHydration } from '@angular/platform-browser';
+// import { DatePipe, DecimalPipe, PercentPipe } from '@angular/common';
 // import { errorInterceptor } from './core/interceptors/error.interceptor';
 // import { successInterceptor } from './core/interceptors/success.interceptor';
-// import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+// // import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
 
 // // PrimeNG Imports
 // import { providePrimeNG } from 'primeng/config';
@@ -122,54 +86,58 @@ export const appConfig: ApplicationConfig = {
 
 // // Import your custom preset
 // // import { MyPreset } from './core/config/my-preset';
-
-// // Interceptors
 // import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
 // import { loggingInterceptor } from './core/interceptors/logging.interceptor';
-
-// // Services
 // import { AuthService } from './modules/auth/services/auth-service';
 // import { DialogService } from 'primeng/dynamicdialog';
-// import { appThemePreset } from '../styles/app-theme.preset';
+// import { TabReuseStrategy } from './Tabbing';
 // // import { ConfirmationService } from '@core/services/confirmationService';
 
 // export const appConfig: ApplicationConfig = {
 //   providers: [
 //     provideHttpClient(
-//       withInterceptors([jwtInterceptor, loggingInterceptor, successInterceptor, errorInterceptor, LoadingInterceptor]),
+//       withInterceptors([jwtInterceptor, successInterceptor, errorInterceptor]),//LoadingInterceptor
 //       withFetch()
 //     ),
 //     provideRouter(routes),
+//     provideClientHydration(
+//       withEventReplay(),
+//       withIncrementalHydration(),
+//       withHttpTransferCacheOptions({
+//         includePostRequests: false,
+//         includeRequestsWithAuthHeaders: false
+//       })
+//     ),
 //     provideZonelessChangeDetection(),
 //     provideAnimationsAsync(),
-//     providePrimeNG({
-//       theme: {
-//         preset: appThemePreset,
-//         options: {
-//           // Must match options.darkModeSelector in the preset
-//           darkModeSelector: '.app-dark',
-//           cssLayer: false,
-//         },
-//       },
-//       ripple: true,
-//     }),
-//     // PRIME NG CONFIGURATION
 //     // providePrimeNG({
 //     //   ripple: true,
 //     //   theme: {
-//     //     // preset: MyPreset,
+//     //     preset: ApexPreset,
 //     //     options: {
-//     //       darkModeSelector: '.theme-dark',
-//     //       cssLayer: {
-//     //         name: 'primeng',
-//     //         order: 'tailwind-base, primeng, tailwind-utilities'
-//     //       }
+//     //       darkModeSelector: '.theme-dark, .theme-neon-eclipse, ...',
+//     //       cssLayer: { name: 'primeng', order: 'theme, base, primeng' }
 //     //     }
 //     //   }
-//     // }),
+//     // })
+//     { provide: RouteReuseStrategy, useClass: TabReuseStrategy },
+
+//     providePrimeNG({
+//       ripple: true,
+//       theme: {
+//         // preset: MyPreset, 
+//         options: {
+//           darkModeSelector: '.theme-dark',
+//           cssLayer: {
+//             name: 'primeng',
+//             order: 'tailwind-base, primeng, tailwind-utilities'
+//           }
+//         }
+//       }
+//     }),
 //     // ConfirmationService
 //     MessageService, ConfirmationService,
-//     DatePipe, DialogService,
+//     DatePipe, DecimalPipe, PercentPipe, DialogService,
 
 //     // ✅ THE MODERN FIX: Using provideAppInitializer
 //     provideAppInitializer(() => {
@@ -178,3 +146,104 @@ export const appConfig: ApplicationConfig = {
 //     })
 //   ]
 // };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import { ApplicationConfig, provideAppInitializer, inject } from '@angular/core';
+// // import { provideZonelessChangeDetection } from '@angular/core';
+// // import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+// // import { provideRouter } from '@angular/router';
+// // import { routes } from './app.routes';
+// // import { provideClientHydration } from '@angular/platform-browser';
+// // import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// // import { DatePipe } from '@angular/common';
+// // import { errorInterceptor } from './core/interceptors/error.interceptor';
+// // import { successInterceptor } from './core/interceptors/success.interceptor';
+// // import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+
+// // // PrimeNG Imports
+// // import { providePrimeNG } from 'primeng/config';
+// // import { MessageService, ConfirmationService } from 'primeng/api';
+
+// // // Import your custom preset
+// // // import { MyPreset } from './core/config/my-preset';
+
+// // // Interceptors
+// // import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
+// // import { loggingInterceptor } from './core/interceptors/logging.interceptor';
+
+// // // Services
+// // import { AuthService } from './modules/auth/services/auth-service';
+// // import { DialogService } from 'primeng/dynamicdialog';
+// // import { appThemePreset } from '../styles/app-theme.preset';
+// // // import { ConfirmationService } from '@core/services/confirmationService';
+
+// // export const appConfig: ApplicationConfig = {
+// //   providers: [
+// //     provideHttpClient(
+// //       withInterceptors([jwtInterceptor, loggingInterceptor, successInterceptor, errorInterceptor, LoadingInterceptor]),
+// //       withFetch()
+// //     ),
+// //     provideRouter(routes),
+// //     provideZonelessChangeDetection(),
+// //     provideAnimationsAsync(),
+// //     providePrimeNG({
+// //       theme: {
+// //         preset: appThemePreset,
+// //         options: {
+// //           // Must match options.darkModeSelector in the preset
+// //           darkModeSelector: '.app-dark',
+// //           cssLayer: false,
+// //         },
+// //       },
+// //       ripple: true,
+// //     }),
+// //     // PRIME NG CONFIGURATION
+// //     // providePrimeNG({
+// //     //   ripple: true,
+// //     //   theme: {
+// //     //     // preset: MyPreset,
+// //     //     options: {
+// //     //       darkModeSelector: '.theme-dark',
+// //     //       cssLayer: {
+// //     //         name: 'primeng',
+// //     //         order: 'tailwind-base, primeng, tailwind-utilities'
+// //     //       }
+// //     //     }
+// //     //   }
+// //     // }),
+// //     // ConfirmationService
+// //     MessageService, ConfirmationService,
+// //     DatePipe, DialogService,
+
+// //     // ✅ THE MODERN FIX: Using provideAppInitializer
+// //     provideAppInitializer(() => {
+// //       const auth = inject(AuthService);
+// //       return auth.initializeFromStorage();
+// //     })
+// //   ]
+// // };
